69f11809a37939e6d795c4f4eb4e0ed1d6ed4aef
Alexis Osueke <alexisosueke@Alexis-Mac-Pro.local>
2022-09-14 18:50:30 -0400

    Carousel ans Nav bar has been updated

--- src/components/HighlightCarousel.vue
+++ src/components/HighlightCarousel.vue
@@ -17,40 +17,38 @@
       <!-- Text slides with image -->
       <b-carousel-slide
         caption="Chi Chi's Market and Restuarant"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://unsplash.com/photos/q_jKuy-cURg"
+       
+        img-src="https://picsum.photos/id/351/3994/2443"
+     
    
       ></b-carousel-slide>
 
       <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
+      <b-carousel-slide 
+      caption="Let's Go Vegan!"
+      img-src="https://picsum.photos/id/488/1772/1181">
       </b-carousel-slide>
 
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template #img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
+      <b-carousel-slide 
+      caption= "Alexis' African Restaurant"
+      img-src="https://picsum.photos/id/395/4080/2720">
       </b-carousel-slide>
 
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
+      <b-carousel-slide 
+      caption= "Kamsi's Caribbean Grill"
+      img-src="https://picsum.photos/id/49/1280/792">
       </b-carousel-slide>
+
+      <b-carousel-slide 
+      caption= "Good For the Soul: Thai Restaurant"
+      img-src="https://picsum.photos/id/686/4462/2975">
+      </b-carousel-slide>
+
+      <b-carousel-slide 
+      caption= "J'adore!: French Bakery"
+      img-src="https://picsum.photos/id/431/5398/3599">
+      </b-carousel-slide>
+
     </b-carousel>
 
    
